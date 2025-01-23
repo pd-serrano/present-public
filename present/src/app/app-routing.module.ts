68c59cdee197a38d75b7f4a9deab4261d06c9bc4
Jhoanna Blanquer <jhoannablanquer@gmail.com>
2025-01-23 21:09:08 +0800

    add routings

--- present/src/app/app-routing.module.ts
+++ present/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // HomeComponent
+import { LoginComponent } from './login/login.component'; // LoginComponent
+import { SignUpComponent } from './sign-up/sign-up.component'; // SignUpComponent
+import { ActivityComponent } from './activity/activity.component'; // ActivityComponent
+import { CreateclassComponent } from './createclass/createclass.component'; // CreateClassComponent
+import { JoinclassComponent } from './joinclass/joinclass.component'; // JoinClassComponent
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },  // Home route
+  { path: 'login', component: LoginComponent },  // Login route
+  { path: 'signup', component: SignUpComponent },  // SignUp route
+  { path: 'activity', component: ActivityComponent },  // Activity route
+  { path: 'createclass', component: CreateclassComponent },  // Create Class route
+  { path: 'joinclass', component: JoinclassComponent },  // Join Class route
 
 
 
