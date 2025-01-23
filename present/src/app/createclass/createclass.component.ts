6168a095fb25d8bb694ef7151dad6f1a917f4703
Jhoanna Blanquer <jhoannablanquer@gmail.com>
2025-01-23 21:02:03 +0800

    add create class

---
+++ present/src/app/createclass/createclass.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createclass',
+  standalone: false,
+  
+  templateUrl: './createclass.component.html',
+  styleUrl: './createclass.component.css'
+})
+export class CreateclassComponent {
+
+}
