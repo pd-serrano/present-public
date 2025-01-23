285339f56b3cdbb4b2342c2c9780b9e8a7d78a93
Jhoanna Blanquer <jhoannablanquer@gmail.com>
2025-01-23 21:10:59 +0800

    updated module

--- present/src/app/app.module.ts
+++ present/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ActivityComponent } from './activity/activity.component';
+import { CreateclassComponent } from './createclass/createclass.component';
+import { JoinclassComponent } from './joinclass/joinclass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent,
+    ActivityComponent,
+    CreateclassComponent,
+    JoinclassComponent
   ],
   imports: [
     BrowserModule,
